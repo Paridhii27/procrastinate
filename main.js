0ced3cadcb1ac0d25b147fda41f11b9d55c6806c
Paridhi <paridhigarg27@gmail.com>
2023-05-12 04:31:49 -0400

    buttons

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.querySelector("#name").addEventListener("keyup", function() {
-		let val = this.value;
-		localStorage.setItem("name", val);
-	})
-    
 	let radios = document.querySelectorAll('.radio');
-	for (var i = radios.length -1; i>= 0; i--){
-		radios[i].addEventListener('change', function() {
-		localStorage.setItem("color", this.value); 
-		})
+	for (var i = radios.length - 1; i >= 0; i--) {
+	  radios[i].addEventListener('change', function() {
+		localStorage.setItem("quiz", this.value); 
+	  });
 	}
-})
+  });;
 
 $(function() {
-	$('a[href*=#]').on('click', function(e) {
+	$('a[href*="#"]').on('click', function(e) {
 	  e.preventDefault();
 	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
-});
+  });
+
