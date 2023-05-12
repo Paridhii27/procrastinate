5d90890cdd43103d79c20b584d3fb34744ba4df4
Paridhi <paridhigarg27@gmail.com>
2023-05-12 00:30:13 -0400

    content addition

--- main.js
+++ main.js
@@ -11,3 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
 		})
 	}
 })
+
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+});
